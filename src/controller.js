e71803393634313bf7144f23bb4bbcd725378273
Nuñez Santiago <nunezdugar@gmail.com>
2024-08-12 12:47:31 -0300

    testing post

--- src/controller.js
+++ src/controller.js
@@ -7,6 +7,14 @@ class LibroController{
 
   }
 
+  async add(req, res){
+    const libro = req.body;
+    const [result] = await pool.query(`INSERT INTO libros(nombre,autor,categoria,año_publicacion,ISBN) VALUES (?,?,?,?,?)`,[libro.nombre,libro.autor,libro.categoria,libro.año_publicacion,libro.ISBN]);
+    res.json({"Id insertado": result.insertid});
+
+
+  }
+
 }
 
 export const libro = new LibroController();
--- src/routes.js
+++ src/routes.js
@@ -4,3 +4,4 @@ import {libro} from './controller.js';
 export const router = Router()
 
 router.get ('/libros',libro.getAll);
+router.post('/libros',libro.add);
